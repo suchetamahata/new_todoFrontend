1f70bf38d0af6a7b027eba92ef56cde3dc0c1920
Sucheta Mahata <suchetamahata>
2021-09-13 11:37:14 +0530

    responsive img and others

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,9 +18,10 @@ const Home =() =>{
                 <p className='text2'>Sign in then login to create todos.</p>
                 <p className='text2'>Happy working !</p>
                 </p>
-                <img src='https://i.pinimg.com/originals/92/81/bc/9281bc09993d71519d896eb5428044ba.jpg' className='himage'></img>
+                <img src='https://i.pinimg.com/originals/92/81/bc/9281bc09993d71519d896eb5428044ba.jpg' className='himage' alt='work desk'></img>
             </div>
             <FormDialog  setUname={setUname} setPassword={setPassword} password={password} uname={uname}></FormDialog>
+            <img src='https://i.pinimg.com/originals/92/81/bc/9281bc09993d71519d896eb5428044ba.jpg' className='himage2' alt='work desk'></img>
         </div>
     )
 }
